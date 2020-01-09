877bb5f9d0090218c6d6b704ab701ee16011b187
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-09 10:22:03 +0200

    gitautosync update 26

--- restaurant.py
+++ restaurant.py
@@ -4,5 +4,8 @@ Order: soup
 soup is 15, total is 35
 Order: elephant
 We are out of elephant today
-Order: <enter>
+Order: < enter >
 Total is 35
+
+
+print(f'blalblalblalalblbb = {my_variable_name}')
