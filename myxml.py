70aba0ad989db2dc4627cb25bb275aa9f8b87ef9
Reuven M. Lerner <reuven@lerner.co.il>
2020-01-09 16:49:19 +0200

    gitautosync update 114

--- myxml.py
+++ myxml.py
@@ -4,22 +4,22 @@
 print(xml('foo'))               # first argument = tagname
 # <foo></foo>
 
-print(xml('foo', 'bar'))        # second (optional) argument = content
-# # # # <foo>bar</foo>
+# print(xml('foo', 'bar'))        # second (optional) argument = content
+# # # # # <foo>bar</foo>
 
-print(xml('a',
-          xml('b',
-              xml('c', 'hello'))))
-# # # # # # <a><b><c>hello</c></b></a>
+# print(xml('a',
+#           xml('b',
+#               xml('c', 'hello'))))
+# # # # # # # <a><b><c>hello</c></b></a>
 
-# # # kwargs become attributes in opening tag
+# # # # kwargs become attributes in opening tag
 
-print(xml('tag', 'text', a=1, b=2, c=3))
+# print(xml('tag', 'text', a=1, b=2, c=3))
 
-# # # # <tag a="1" b="2" c="3">text</tag>
+# # # # # <tag a="1" b="2" c="3">text</tag>
 
-print(xml('tag', 'text', a=1, b=2))
-# # # # <tag a="1" b="2">text</tag>
+# print(xml('tag', 'text', a=1, b=2))
+# # # # # <tag a="1" b="2">text</tag>
 
-print(xml('tag', a=1, b=2))
-# # # # # <tag a="1" b="2"></tag>
+# print(xml('tag', a=1, b=2))
+# # # # # # <tag a="1" b="2"></tag>
